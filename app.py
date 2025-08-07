2b1b846670cd5a8c697a35a6633a1aba1737a0bb
Wellington Lima <wellingtonlima.jt@gmail.com>
2025-08-07 12:00:20 -0300

    Ajustes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime, timedelta
 
 MULHERES = ['Wladia', 'Vânia', 'Ivonete', 'Dirce', 'Kelly', 'Ana Paula', 'Daisy', 'Júlia', 'Andréa', 'Thais']
 
@@ -15,7 +16,7 @@ def gerar_rodadas(lista_mulheres):
     rodadas = {}
     
     for i in range(num_rodadas):
-        nome_rodada = f'rodada{i+1}'
+        nome_rodada = f'sessao{i+1}'
         rodadas[nome_rodada] = []
         
         for j in range(metade):
@@ -29,10 +30,23 @@ def gerar_rodadas(lista_mulheres):
     
     return rodadas
 
-rodadas = gerar_rodadas(MULHERES.copy())  # Usamos copy para não modificar a lista original
+# Gerar as datas das sessões (começando em 12/08/2024)
+def gerar_datas_sessoes(num_sessoes, data_inicio="12/08/2024"):
+    data = datetime.strptime(data_inicio, "%d/%m/%Y")
+    datas = []
+    for i in range(num_sessoes):
+        datas.append(data + timedelta(days=7*i))
+    return [d.strftime("%d/%m") for d in datas]
+
+rodadas = gerar_rodadas(MULHERES.copy())
+datas_sessoes = gerar_datas_sessoes(len(rodadas))
 
 st.title('Duplas de Oração - MEM')
 
-for nome_rodada, duplas in rodadas.items():
-    st.subheader(nome_rodada.capitalize().replace('Rodada', 'Rodada '))
+for idx, (nome_rodada, duplas) in enumerate(rodadas.items()):
+    # Formata o título da sessão com a data
+    numero_sessao = idx + 1
+    st.subheader(f'Sessão {numero_sessao} - {datas_sessoes[idx]}')
+    
+    # Exibe a tabela sem índices
     st.table(duplas)
